8da4debb05e385b5a5f2bf88270814fb4f52f88a
Jyrki Keisala <jyrki.keisala@gmail.com>
2014-04-15 09:59:34 +0300

    Permission group support added.

--- src/com/github/etsija/statistics/Statistics.java
+++ src/com/github/etsija/statistics/Statistics.java
@@ -19,7 +19,10 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.milkbowl.vault.permission.Permission;
 
 public class Statistics extends JavaPlugin {
 
@@ -31,9 +34,22 @@ public class Statistics extends JavaPlugin {
 	HelperMethods helper = new HelperMethods();				// Helper class with various methods
 	private int _showNewestUsers;
 	private int _showLoginsPerUser;
+	public static Permission permission = null;
+	public enum Groups {
+	    newbie,
+	    prisoner,
+	    guest,
+	    normal,
+	    hungryplayer,
+	    survivor,
+	    sb,
+	    sy,
+	    vip,
+	    moderator,
+	    admin
+	}
 	
 	public void onEnable() {
-		
 		
 		// Initialize the configuration files
 		// Note that so far, they're only virtual, not real files yet
@@ -72,6 +88,7 @@ public class Statistics extends JavaPlugin {
 							 ".sqlite");
 		sqlDb.createTables();
 		new PlayerListener(this);
+		setupPermissions();
 		_log.info("[Statistics] enabled!");
 	}
 	
@@ -92,7 +109,7 @@ public class Statistics extends JavaPlugin {
 			
 			// /stats user [playername] | /stats user [playername] {n}
 			if (args[0].equalsIgnoreCase("user") &&
-				sender.hasPermission("statistics.stats.player")) {
+				sender.hasPermission("statistics.stats.user")) {
 				if (args.length < 2) {
 					sender.sendMessage("[Statistics] Usage: /stats user [playername] {n}");
 				} else {
@@ -110,7 +127,7 @@ public class Statistics extends JavaPlugin {
 						// Find all players who have ever played on this server
 						OfflinePlayer [] allPlayers = Bukkit.getServer().getOfflinePlayers();
 						for (OfflinePlayer thisPlayer : allPlayers) {
-							String playerName = thisPlayer.getName();
+							String playerName = thisPlayer.getName();	
 							// If the player is offline but has played on this server
 							if (playerName.equalsIgnoreCase(args[1])) {
 								if (args.length == 2) {
@@ -126,7 +143,7 @@ public class Statistics extends JavaPlugin {
 						return true;
 					}
 				}
-			
+				
 			// /stats newest | /stats newest {n}
 			} else if (args[0].equalsIgnoreCase("newest") &&
 					   sender.hasPermission("statistics.stats.newest")) {
@@ -134,7 +151,7 @@ public class Statistics extends JavaPlugin {
 					List<String> newestList = sqlDb.readNewestLogins(_showNewestUsers);
 					sender.sendMessage("[Statistics] Latest " + newestList.size() + " visitors:");
 					for (String str : newestList) {
-						sender.sendMessage(ChatColor.DARK_GREEN + str);
+						showNewestStats(sender, str);
 					}
 					return true;
 				} else if (args.length == 2) {
@@ -142,7 +159,7 @@ public class Statistics extends JavaPlugin {
 					List<String> newestList = sqlDb.readNewestLogins(howMany);
 					sender.sendMessage("[Statistics] Latest " + newestList.size() + " visitors:");
 					for (String str : newestList) {
-						sender.sendMessage(ChatColor.DARK_GREEN + str);
+						showNewestStats(sender, str);
 					}
 					return true;
 				}
@@ -151,11 +168,12 @@ public class Statistics extends JavaPlugin {
 			} else if (args[0].equalsIgnoreCase("self") &&
 					   sender.hasPermission("statistics.stats.self")) {
 				Player player = (Player) sender;
+				String playerName = player.getName();
 				if (args.length < 2) {
-					showPlayerStats(sender, player.getName(), true, _showLoginsPerUser);
+					showPlayerStats(sender, playerName, true, _showLoginsPerUser);
 					return true;
 				} else if (args.length == 2) {
-					showPlayerStats(sender, player.getName(), true, Integer.parseInt(args[1]));
+					showPlayerStats(sender, playerName, true, Integer.parseInt(args[1]));
 					return true;
 				}
 			}
@@ -167,15 +185,25 @@ public class Statistics extends JavaPlugin {
 	// Some helper methods
 	//////////////////////////////////////
 	
+	// Hook up to the Permissions plugin used (PEX; to read the Permission group of a player later on)
+	private boolean setupPermissions()
+    {
+        RegisteredServiceProvider<Permission> permissionProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
+        if (permissionProvider != null) {
+            permission = permissionProvider.getProvider();
+        }
+        return (permission != null);
+    }
+	
 	// Function to show the stats of one player, whether online or offline
 	public void showPlayerStats(CommandSender sender, String playerName, boolean isOnline, int nLogins) {
 		if (isOnline) {
 			int onlineTime = sqlDb.getOnlineTime(playerName);
-			sender.sendMessage("[Statistics] '" + playerName + "' is"
+			sender.sendMessage("[Statistics] '" + getColor(playerName) + playerName + ChatColor.WHITE + "' is"
 							 + ChatColor.GREEN + " [ONLINE since "
 						     + helper.timeFormatted(onlineTime) + "]");
 		} else {
-			sender.sendMessage("[Statistics] '" + playerName + "' is"
+			sender.sendMessage("[Statistics] '" + getColor(playerName) + playerName + ChatColor.WHITE + "' is"
 					         + ChatColor.RED + " [OFFLINE]");
 		}
 		List<String> loginList = sqlDb.readLoginInfo(playerName, nLogins);
@@ -184,7 +212,80 @@ public class Statistics extends JavaPlugin {
 		}
 		sender.sendMessage("Logins: " + sqlDb.getTotalLogins(playerName)
 						 + " Tot: " + helper.timeFormatted(sqlDb.getTotalPlaytime(playerName))
-						 + " Avg: " + helper.timeFormatted(sqlDb.getAvgPlaytime(playerName)));
+						 + " Avg: " + helper.timeFormatted(sqlDb.getAvgPlaytime(playerName))
+						 + " " + getColor(playerName) + permission.getPrimaryGroup("", playerName));
+	}
+	
+	// Function to show one line of the newest players list
+	public void showNewestStats(CommandSender sender, String theLine) {
+		String[] temp = theLine.split("\\s+");
+		String date = temp[0];
+		String time = temp[1];
+		String onlineTime = "";
+		String playerName = "";
+		if (temp.length == 4) {  // Player is offline
+			onlineTime = temp[2];
+			playerName = temp[3];
+		} else if (temp.length == 6) {  // Player is online
+			onlineTime = "[ ONLINE ]";
+			playerName = temp[5];
+		}
+		
+		if (onlineTime.contains("ONLINE")) {
+			sender.sendMessage(ChatColor.DARK_GREEN + date + " "
+							 + time + " "
+							 + onlineTime + " "
+							 + getColor(playerName) + playerName);
+		} else {
+			sender.sendMessage(ChatColor.DARK_GREEN + date + " "
+					 		 + time + " "
+					 		 + ChatColor.RED + onlineTime + " "
+					 		 + getColor(playerName) + playerName);
+		}
+	}
+	
+	// Return the correct chat color based on the group of the player
+	ChatColor getColor(String playerName) {
+		ChatColor retColor = ChatColor.WHITE;
+		String temp  = permission.getPrimaryGroup("", playerName);
+		Groups group = Groups.valueOf(temp.toLowerCase());
+		
+		switch (group) {
+	    	case newbie:
+	    		retColor = ChatColor.YELLOW;
+	    		break;
+	    	case prisoner:
+	    		retColor = ChatColor.DARK_GRAY;
+	    		break;
+	    	case guest:
+	    		retColor = ChatColor.GRAY;
+	    		break;
+	    	case normal:
+	    		retColor = ChatColor.GREEN;
+	    		break;
+	    	case hungryplayer:
+	    	case survivor:
+	    		retColor = ChatColor.LIGHT_PURPLE;
+	    		break;
+	    	case sb:
+	    		retColor = ChatColor.AQUA;
+	    		break;
+	    	case sy:
+	    		retColor = ChatColor.YELLOW;
+	    		break;
+	    	case vip:
+	    		retColor = ChatColor.DARK_AQUA;
+	    		break;
+	    	case moderator:
+	    		retColor = ChatColor.DARK_PURPLE;
+	    		break;
+	    	case admin:
+	    		retColor = ChatColor.RED;
+	    		break;
+	    	default:
+	    		retColor = ChatColor.WHITE;
+		}
+		return retColor;
 	}
 	
 	//////////////////////////////////////
